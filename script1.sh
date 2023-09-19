63c00b7f2dd4ce7838828239f8ecf7442b1e2467
imjustleaving <109609649+imjustleaving@users.noreply.github.com>
2023-09-19 08:58:09 -0400

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -715,8 +715,7 @@ sudo docker start unpackerr
 #recyclarr setup
 
 sudo docker exec recyclarr recyclarr config create
-sudo chmod 777 $CONFIG_PATH/recyclarr/recyclarr.yml
-
+sudo -u apps chmod 777 $CONFIG_PATH/recyclarr/recyclarr.yml
 
 cat <<EOF > $CONFIG_PATH/recyclarr/recyclarr.yml
 # Configuration specific to Sonarr
