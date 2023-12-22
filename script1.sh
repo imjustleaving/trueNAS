c9ac8cc113894d2b6675476fd1df6d02f0405b3b
imjustleaving <109609649+imjustleaving@users.noreply.github.com>
2023-12-22 08:17:36 -0500

    updated recyclarr settings

--- script1.sh
+++ script1.sh
@@ -729,207 +729,84 @@ sonarr:
       type: series
 
     # Release profiles from the guide to sync to Sonarr v3 (Sonarr v4 does not use this!)
-    # Use 'recyclarr list release-profiles' for values you can put here.
+    # Use `recyclarr list release-profiles` for values you can put here.
     # https://trash-guides.info/Sonarr/Sonarr-Release-Profile-RegEx/
     release_profiles:
       # Series
       - trash_ids:
-          - 76e060895c5b8a765c310933da0a5357 # Optionals
-          - 71899E6C303A07AF0E4746EFF9873532 # P2P Groups + Repack/Proper
-          - d428eda85af1df8904b4bbe4fc2f537c # Anime - First release profile
           - EBC725268D687D588A20CBC5F97E538B # Low Quality Groups
           - 1B018E0C53EC825085DD911102E2CA36 # Release Sources (Streaming Service)
-          - 6cd9e10bb5bb4c63d2d7cd3279924c7b # Anime - Second release profile
+          - 71899E6C303A07AF0E4746EFF9873532 # P2P Groups + Repack/Proper
+          - 76e060895c5b8a765c310933da0a5357 # Optionals
+      # Anime (Uncomment below if you want it)
+      #- trash_ids:
+      #    - d428eda85af1df8904b4bbe4fc2f537c # Anime - First release profile
+      #    - 6cd9e10bb5bb4c63d2d7cd3279924c7b # Anime - Second release profile
 
 # Configuration specific to Radarr.
 radarr:
-  movies:
-    # Set the URL/API Key to your actual instance
+ uhd-bluray-web:
     base_url: http://radarr:7878
     api_key: $RADARR_API_KEY
 
-    # Which quality definition in the guide to sync to Radarr. Only choice right now is 'movie'
-    quality_definition:
-      type: movie
+    include:
+     # Comment out any of the following includes to disable them
+     - template: radarr-quality-definition-movie
+     - template: radarr-quality-profile-uhd-bluray-web
+     - template: radarr-custom-formats-uhd-bluray-web
+     - template: radarr-quality-definition-movie
+     - template: radarr-quality-profile-hd-bluray-web
+     - template: radarr-custom-formats-hd-bluray-web
 
-    # Set to 'true' to automatically remove custom formats from Radarr when they are removed from
-    # the guide or your configuration. This will NEVER delete custom formats you manually created!
-    delete_old_custom_formats: false
-
+# Custom Formats: https://recyclarr.dev/wiki/yaml/config-reference/custom-formats/
     custom_formats:
-      # A list of custom formats to sync to Radarr.
-      # Use 'recyclarr list custom-formats radarr' for values you can put here.
-      # https://trash-guides.info/Radarr/Radarr-collection-of-custom-formats/
-      - trash_ids:
-          # [No Category]
-          - 820b09bb9acbfde9c35c71e0e565dad8 # 1080p
-          - fb392fb0d61a010ae38e49ceaa24a1ef # 2160p
-          - b2be17d608fc88818940cd1833b0b24c # 720p
-          - 5153ec7413d9dae44e24275589b5e944 # BHDStudio
-          - 0a3f082873eb454bde444150b70253cc # Extras
-          - e098247bc6652dd88c76644b275260ed # FLUX
-          - ff5bc9e8ce91d46c997ca3ac6994d6f8 # FraMeSToR
-          - 8cd3ac70db7ac318cf9a0e01333940a4 # SiC
+     # Audio
+     - trash_ids:
+         # Uncomment the next section to enable Advanced Audio Formats
+         # - 496f355514737f7d83bf7aa4d24f8169 # TrueHD Atmos
+         # - 2f22d89048b01681dde8afe203bf2e95 # DTS X
+         # - 417804f7f2c4308c1f4c5d380d4c4475 # ATMOS (undefined)
+         # - 1af239278386be2919e1bcee0bde047e # DD+ ATMOS
+         # - 3cafb66171b47f226146a0770576870f # TrueHD
+         # - dcf3ec6938fa32445f590a4da84256cd # DTS-HD MA
+         # - a570d4a0e56a2874b64e5bfa55202a1b # FLAC
+         # - e7c2fcae07cbada050a0af3357491d7b # PCM
+         # - 8e109e50e0a0b83a5098b056e13bf6db # DTS-HD HRA
+         # - 185f1dd7264c4562b9022d963ac37424 # DD+
+         # - f9f847ac70a0af62ea4a08280b859636 # DTS-ES
+         # - 1c1a4c5e823891c75bc50380a6866f73 # DTS
+         # - 240770601cc226190c367ef59aba7463 # AAC
+         # - c2998bd0d90ed5621d8df281e839436e # DD
+       quality_profiles:
+         - name: UHD Bluray + WEB
 
-          # Anime
-          - fb3ccc5d5cc8f77c9055d4cb4561dded # Anime BD Tier 01 (Top SeaDex Muxers)
-          - 66926c8fa9312bc74ab71bf69aae4f4a # Anime BD Tier 02 (SeaDex Muxers)
-          - fa857662bad28d5ff21a6e611869a0ff # Anime BD Tier 03 (SeaDex Muxers)
-          - f262f1299d99b1a2263375e8fa2ddbb3 # Anime BD Tier 04 (SeaDex Muxers)
-          - ca864ed93c7b431150cc6748dc34875d # Anime BD Tier 05 (Remuxes)
-          - 9dce189b960fddf47891b7484ee886ca # Anime BD Tier 06 (FanSubs)
-          - 1ef101b3a82646b40e0cab7fc92cd896 # Anime BD Tier 07 (P2P/Scene)
-          - 6115ccd6640b978234cc47f2c1f2cadc # Anime BD Tier 08 (Mini Encodes)
-          - b0fdc5897f68c9a68c70c25169f77447 # Anime LQ Groups
-          - 06b6542a47037d1e33b15aa3677c2365 # Anime Raws
-          - 8167cffba4febfb9a6988ef24f274e7e # Anime Web Tier 01 (Muxers)
-          - 8526c54e36b4962d340fce52ef030e76 # Anime Web Tier 02 (Top FanSubs)
-          - de41e72708d2c856fa261094c85e965d # Anime Web Tier 03 (Official Subs)
-          - 9edaeee9ea3bcd585da9b7c0ac3fc54f # Anime Web Tier 04 (Official Subs)
-          - 22d953bbe897857b517928f3652b8dd3 # Anime Web Tier 05 (FanSubs)
-          - a786fbc0eae05afe3bb51aee3c83a9d4 # Anime Web Tier 06 (FanSubs)
-          - 60f6d50cbd3cfc3e9a8c00e3a30c3114 # VRV
-          - c259005cbaeb5ab44c06eddb4751e70c # v0
-          - 5f400539421b8fcf71d51e6384434573 # v1
-          - 3df5e6dfef4b09bb6002f732bed5b774 # v2
-          - db92c27ba606996b146b57fbe6d09186 # v3
-          - d4e5e842fad129a3c097bdb2d20d31a0 # v4
+     # Movie Versions
+     - trash_ids:
+         - 9f6cbff8cfe4ebbc1bde14c7b7bec0de # IMAX Enhanced
+       quality_profiles:
+         - name: UHD Bluray + WEB
+           # score: 0 # Uncomment this line to disable prioritised IMAX Enhanced releases
 
-          # Anime Optional
-          - a5d148168c4506b55cf53984107c396e # 10bit
-          - 4a3b087eea2ce012fcc1ce319259a3be # Anime Dual Audio
-          - b23eae459cc960816f2d6ba84af45055 # Dubs Only
-          - 064af5f084a0a24458cc8ecd3220f93f # Uncensored
+     # Optional
+     - trash_ids:
+         # Comment out the next line if you and all of your users' setups are fully DV compatible
+         - 923b6abef9b17f937fab56cfcf89e1f1 # DV (WEBDL)
+         # HDR10Plus Boost - Uncomment the next line if any of your devices DO support HDR10+
+         # - b17886cb4158d9fea189859409975758 # HDR10Plus Boost
+       quality_profiles:
+         - name: UHD Bluray + WEB
 
-          # Audio Advanced #1
-          - 417804f7f2c4308c1f4c5d380d4c4475 # ATMOS (undefined)
-          - 185f1dd7264c4562b9022d963ac37424 # DD+
-          - 1af239278386be2919e1bcee0bde047e # DD+ ATMOS
-          - 1c1a4c5e823891c75bc50380a6866f73 # DTS
-          - 2f22d89048b01681dde8afe203bf2e95 # DTS X
-          - f9f847ac70a0af62ea4a08280b859636 # DTS-ES
-          - dcf3ec6938fa32445f590a4da84256cd # DTS-HD MA
-          - 3cafb66171b47f226146a0770576870f # TrueHD
-          - 496f355514737f7d83bf7aa4d24f8169 # TrueHD ATMOS
+     - trash_ids:
+         - 9c38ebb7384dada637be8899efa68e6f # SDR
+       quality_profiles:
+         - name: UHD Bluray + WEB
+           # score: 0 # Uncomment this line to allow SDR releases
 
-          # Audio Advanced #2
-          - 240770601cc226190c367ef59aba7463 # AAC
-          - c2998bd0d90ed5621d8df281e839436e # DD
-          - 8e109e50e0a0b83a5098b056e13bf6db # DTS-HD HRA
-          - a570d4a0e56a2874b64e5bfa55202a1b # FLAC
-          - 6ba9033150e7896bdc9ec4b44f2b230f # MP3
-          - a061e2e700f81932daf888599f8a8273 # Opus
-          - e7c2fcae07cbada050a0af3357491d7b # PCM
-
-          # Audio Channels
-          - b124be9b146540f8e62f98fe32e49a2a # 1.0 Mono
-          - 89dac1be53d5268a7e10a19d3c896826 # 2.0 Stereo
-          - 205125755c411c3b8622ca3175d27b37 # 3.0 Sound
-          - 373b58bd188fc00c817bd8c7470ea285 # 4.0 Sound
-          - 77ff61788dfe1097194fd8743d7b4524 # 5.1 Surround
-          - 6fd7b090c3f7317502ab3b63cc7f51e3 # 6.1 Surround
-          - e77382bcfeba57cb83744c9c5449b401 # 7.1 Surround
-          - f2aacebe2c932337fe352fa6e42c1611 # 9.1 Surround
-
-
-          # HDR Formats
-          - 58d6a88f13e2db7f5059c41047876f00 # DV
-          - e23edd2482476e595fb990b12e7c609c # DV HDR10
-          - 55d53828b9d81cbe20b02efd00aa0efd # DV HLG
-          - a3e19f8f627608af0211acd02bf89735 # DV SDR
-          - e61e28db95d22bedcadf030b8f156d96 # HDR
-          - 2a4d9069cc1fe3242ff9bdaebed239bb # HDR (undefined)
-          - dfb86d5941bc9075d6af23b09c2aeecd # HDR10
-          - b974a6cd08c1066250f1f177d7aa1225 # HDR10+
-          - 9364dd386c9b4a1100dde8264690add7 # HLG
-          - 08d6d8834ad9ec87b1dc7ec8148e7a1f # PQ
-
-          # HQ Release Groups
-          - ed27ebfef2f323e964fb1f61391bcb35 # HD Bluray Tier 01
-          - c20c8647f2746a1f4c4262b0fbbeeeae # HD Bluray Tier 02
-          - 5608c71bcebba0a5e666223bae8c9227 # HD Bluray Tier 03
-          - 4d74ac4c4db0b64bff6ce0cffef99bf0 # UHD Bluray Tier 01
-          - a58f517a70193f8e578056642178419d # UHD Bluray Tier 02
-          - e71939fae578037e7aed3ee219bbe7c1 # UHD Bluray Tier 03
-          - c20f169ef63c5f40c2def54abaf4438e # WEB Tier 01
-          - 403816d65392c79236dcb6dd591aeda4 # WEB Tier 02
-          - af94e0fe497124d1f9ce732069ec8c3b # WEB Tier 03
-
-          # Misc
-          - 9de657fd3d327ecf144ec73dfe3a3e9a # Dutch Groups
-          - 0d91270a7255a1e388fa85e959f359d8 # FreeLeech
-          - ff86c4326018682f817830ced463332b # MPEG2
-          - 4b900e171accbfb172729b63323ea8ca # Multi
-          - e7718d7a3ce595f289bfee26adc178f5 # Repack/Proper
-          - ae43b294509409a6a13919dedd4764c4 # Repack2
-          - 2899d84dc9372de3408e6d8cc18e9666 # x264
-          - 9170d55c319f4fe40da8711ba9d8050d # x265
-
-          # Movie Versions
-          - eca37840c13c6ef2dd0262b141a5482f # 4K Remaster
-          - e0c07d59beb37348e975a930d5e50319 # Criterion Collection
-          - 0f12c086e289cf966fa5948eac571f44 # Hybrid
-          - eecf3a857724171f968a66cb5719e152 # IMAX
-          - 9f6cbff8cfe4ebbc1bde14c7b7bec0de # IMAX Enhanced
-          - 9d27d9d2181838f76dee150882bdc58c # Masters of Cinema
-          - 09d9dd29a0fc958f9796e65c2a8864b4 # Open Matte
-          - 570bc9ebecd92723d2d21500f4be314c # Remaster
-          - 957d0f44b592285f26449575e8b1167e # Special Edition
-          - e9001909a4c88013a359d0b9920d7bea # Theatrical Cut
-          - db9b4c4b53d312a3ca5f1378f6440fc9 # Vinegar Syndrome
-
-          # Optional
-          - cae4ca30163749b891686f95532519bd # AV1
-          - b6832f586342ef70d9c128d40c07b872 # Bad Dual Groups
-          - f700d29429c023a5734505e77daeaea7 # DV (FEL)
-          - 923b6abef9b17f937fab56cfcf89e1f1 # DV (WEBDL)
-          - 90cedc1fea7ea5d11298bebd3d1d3223 # EVO (no WEBDL)
-          - b17886cb4158d9fea189859409975758 # HDR10+ Boost
-          - 73613461ac2cea99d52c4cd6e177ab82 # HFR
-          - c465ccc73923871b3eb1802042331306 # Line/Mic Dubbed
-          - ae9b7c9ebde1f3bd336a8cbd1ec4c5e5 # No-RlsGroup
-          - 7357cf5161efbf8c4d5d0c30b4815ee2 # Obfuscated
-          - 5c44f52a8714fdd79bb4d98e2673be1f # Retags
-          - 9c38ebb7384dada637be8899efa68e6f # SDR
-          - f537cf427b64c38c8e36298f657e4828 # Scene
-          - ae4cfaa9283a4f2150ac3da08e388723 # VP9
-          - 839bea857ed2c0a8e084f3cbdbd65ecb # x265 (no HDR/DV)
-
-          # Streaming Services
-          - b3b3a6ac74ecbd56bcdbefa4799fb9df # AMZN
-          - 40e9380490e748672c2522eaaeb692f7 # ATVP
-          - cc5e51a9e85a6296ceefe097a77f12f4 # BCORE
-          - f6ff65b3f4b464a79dcc75950fe20382 # CRAV
-          - 16622a6911d1ab5d5b8b713d5b0036d4 # CRiT
-          - 84272245b2988854bfb76a16e60baea5 # DSNP
-          - 509e5f41146e278f9eab1ddaceb34515 # HBO
-          - 5763d1b0ce84aff3b21038eea8e9b8ad # HMAX
-          - 526d445d4c16214309f0fd2b3be18a89 # Hulu
-          - 2a6039655313bf5dab1e43523b62c374 # MA
-          - 6a061313d22e51e0f25b7cd4dc065233 # MAX
-          - 170b1d363bd8516fbf3a3eb05d4faff6 # NF
-          - fbca986396c5e695ef7b2def3c755d01 # OViD
-          - c9fd353f8f5f1baf56dc601c4cb29920 # PCOK
-          - e36a0ba1bc902b26ee40818a1d59b8bd # PMTP
-          - bf7e73dd1d85b12cc527dc619761c840 # Pathe
-          - c2863d2a50c9acad1fb50e53ece60817 # STAN
-
-          # Unwanted
-          - b8cd450cbfa689c0259a01d9e29ba3d6 # 3D
-          - ed38b889b31be83fda192888e2286d83 # BR-DISK
-          - 90a6f9a284dff5103f6346090e6280c8 # LQ
-          - bfd8eb01832d646a0a89c4deb46f8564 # Upscaled
-          - dc98083864ea246d05a42df0d05f81cc # x265 (HD)
-
-        # Uncomment the below properties to specify one or more quality profiles that should be
-        # updated with scores from the guide for each custom format. Without this, custom formats
-        # are synced to Radarr but no scores are set in any quality profiles.
-        quality_profiles:
-          - name: HD-1080p
-          - name: Ultra-HD
-        #    #score: -9999 # Optional score to assign to all CFs. Overrides scores in the guide.
-        #    #reset_unmatched_scores: true # Optionally set other scores to 0 if they are not listed in 'names' above.
-
+     - trash_ids:
+         - 9f6cbff8cfe4ebbc1bde14c7b7bec0de # IMAX Enhanced
+       quality_profiles:
+         - name: HD Bluray + WEB
+           # score: 0 # Uncomment this line to disable prioritised IMAX Enhanced releases
 EOF
 
 sleep 1
